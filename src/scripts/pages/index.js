02507ef4e668065dd21e9d9aeaa2b105928e7aa7
Maxime Dulieu <staphir@gmail.com>
2023-11-10 18:07:21 +0100

    header without search button animation

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+
+/**
+ * Add event to change color of search button and it icon 
+ */
+// function changeSearchButtonColor() {
+    
+// }
+
+// document.querySelector('search-button').addEventListener('mouseover', changeSearchButtonColor);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yellow'    : '#FFD15B',
+        'black'     : '#1B1B1B',
+        'grey'      : '#7A7A7A',
+        'grey-light': '#C6C6C6',
+      },
+      fontFamily: {
+        'body'  : ['Manrope', 'sans-serif'],
+        'title' : ['Anton', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
